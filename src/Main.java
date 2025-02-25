05593a4e4ad105327b417a0392be776fdd455b68
Konstantin S. <kostellos84@mail.ru>
2025-02-25 11:17:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 10000;
+        int bonus_miles = (ticket_price / 20);
+        System.out.println(bonus_miles + " Бонусных(ая) миль(я) зачислено(а)");
+
+
+    }
+}
