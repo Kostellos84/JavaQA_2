fc3772cc87aed8b036de64c6bff40ca9fb0fe6a3
Konstantin S. <kostellos84@mail.ru>
2025-02-27 09:52:54 +0300

    Changes main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 10000;
-        int bonus_miles = (ticket_price / 20);
-        System.out.println(bonus_miles + " Бонусных(ая) миль(я) зачислено(а)");
+        int ticketPrice = 10000;
+        int bonusMiles = (ticketPrice / 20);
+        System.out.println(bonusMiles + " Бонусных(ая) миль(я) зачислено(а)");
 
 
     }
